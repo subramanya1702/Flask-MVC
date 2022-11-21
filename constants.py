1b8322226e89e33fdd8c72071fe1a04460eb09ee
Subramanya1702 <keshavas@oregonstate.edu>
2022-11-21 13:01:57 -0800

    Basic API setup

---
+++ constants.py
@@ -0,0 +1,9 @@
+ALGORITHMS = ["RS256"]
+DOMAIN = "portfolio-keshavas.us.auth0.com"
+CLIENT_ID = "Ps2zh5iBStLlCAKoNkL2smEdAMQy50od"
+CLIENT_SECRET = "JatKLbn2cSPSWtLcfU4XMrRYBsV_Z218tQg_QZoBDIYNwCb4AHUuV-fK2g6oIuc9"
+SECRET_KEY = "8506b5a268cf82a80b7de19efc4289be0568a641ddd5fe47bfc8e442a4f49d7c"
+
+boat_not_found_error = "No boat with this boat_id exists"
+boat_not_owned_error = "Boat with this boat_id is owned by someone else"
+
